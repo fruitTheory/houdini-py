5d5834cb4f3d4f15d7c4922c9426db9be8bf84e0
Derwin <derwinprince61@gmail.com>
2024-03-03 13:21:36 -0500

    Add Signals handles for button

--- solaris_utils/file_selector.py
+++ solaris_utils/file_selector.py
@@ -6,6 +6,10 @@ from PySide2 import QtCore, QtUiTools, QtWidgets
 # General basic Ui for selecting files
 class FileSelector(QtWidgets.QWidget):
 
+    # Global Signal that passes type list
+    signal_files = QtCore.Signal(list)
+    signal_directory = QtCore.Signal(str)
+
     # Create detached widget
     def __init__(self) -> None:
         # Init Qwidget
@@ -16,27 +20,29 @@ class FileSelector(QtWidgets.QWidget):
         ui_filename = 'ui/file_importer.ui'
         ui_file = os.path.join(current_dir, ui_filename)
 
-        self.setWindowTitle('File Importer')
         # Pull ui file and set parent to houdini's window
+        self.setWindowTitle('File Importer')
         self.ui = QtUiTools.QUiLoader().load(ui_file, parentWidget = self) # QDialog
         self.setParent(hou.ui.mainQtWindow(), QtCore.Qt.Window)
 
-        # Setup button one - parent to object - connect btn to function
-        button = self.ui.findChild(QtWidgets.QPushButton, 'pushButton')
-        button.setFocusPolicy(QtCore.Qt.NoFocus)
-        button.setParent(self)
-        self.connect(button, QtCore.SIGNAL('clicked()'), self.select_directory)
+        # Initialize btn - Parent to object - Connect btn to function
+        self.button = self.ui.findChild(QtWidgets.QPushButton, 'pushButton')
+        self.button.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.button.setParent(self)
+        self.button.clicked.connect(self.select_directory)
+
 
     # Select a directory and returns files inside of it
     def select_directory(self) -> list[str]:
         hui = hou.ui # Houdini interal ui actions
         directory = hui.selectFile(file_type=hou.fileType.Directory)
-        directory = hou.expandString(directory) # Unpack variables in case
+        directory = hou.expandString(directory) # Unpack hou variables
         dir_files = os.listdir(directory)
-        print(dir_files)
 
-        return dir_files
+        # Emit instanced Signal: value is list of files
+        self.signal_files.emit(dir_files)
+        self.signal_directory.emit(directory)
+        
     
     def showui(self) -> None:
         self.show()
-
--- solaris_utils/texture_convert.py
+++ solaris_utils/texture_convert.py
@@ -1,16 +1,40 @@
 import hou
 import os
 
-from solaris_utils.utility import node_utils
 from solaris_utils import file_selector as fs
+from importlib import reload
+reload(fs)
 
 # Import textures from disk and convert to MaterialX network
-class TextureImport:
+class TextureImport(fs.FileSelector):
+
+    files = []
+    directory = ""
+
     def __init__(self) -> None:
-        pass
+        super().__init__()
+
+        # Receive signal from button emitting
+        self.signal_files.connect(self.handle_file_signal)
+        self.signal_directory.connect(self.handle_directory_signal)
+
+        # Add new clicked connection to button
+        self.button.clicked.connect(self.convert_texture)
 
     def import_textures(self) -> str:
-        get_files = fs.FileSelector()
-        files = get_files.showui()
-        if(files != None): print(files)
+        self.showui()
+
+    def handle_file_signal(self, value):
+        self.files = value
+        print(self.files)
+
+    def handle_directory_signal(self, value):
+        self.directory = value
+        print(self.directory)
+
+    def convert_texture(self):
+        print('Converting..')
+
+
+
 
